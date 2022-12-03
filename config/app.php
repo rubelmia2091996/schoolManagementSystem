b08e9773e19955dc972c559d74bbf5707c076581
Rubel Mia <rubel.mia2091996@gmail.com>
2022-12-04 00:09:14 +0600

    Laravel Excel
    maatwebsite/excel is a package for laravel excel

--- config/app.php
+++ config/app.php
@@ -198,6 +198,9 @@ return [
 
         Spatie\Permission\PermissionServiceProvider::class,
 
+
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+
     ],
 
     /*
@@ -213,6 +216,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ])->toArray(),
 
 ];
