b8fc79ff8175af9c2cab7e5d07edeb76ce1c5760
Rubel Mia <rubel.mia2091996@gmail.com>
2022-12-05 00:51:55 +0600

    admin LTE Impliment
    set up admin panel design

---
+++ resources/views/adminPanel/dashboard.blade.php
@@ -0,0 +1,20 @@
+@extends('adminPanel.master')
+@section('tittle')
+ 
+@endsection
+
+@section('css')
+    
+@endsection
+
+@section('content')
+
+<div class="content-wrapper">
+
+</div>
+
+@endsection
+
+@section('js')
+    
+@endsection
